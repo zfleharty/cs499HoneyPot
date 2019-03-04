41ff8aee71aa653324d6a81cea87d6058c64ec59
zfleharty <zfleharty@unm.edu>
2019-03-04 06:56:17 -0500

    added init.sql file to automatically create table in database and added Dockerfile to layer init.sql file into mariadb-image

--- HoneyPy/loggers/mariadb/honeypy_mariadb.py
+++ HoneyPy/loggers/mariadb/honeypy_mariadb.py
@@ -1,20 +1,6 @@
 import mysql.connector
 
 
-# TABLES = {}
-# TABLES['connections'] = (
-#     "CREATE TABLE `connections` ("
-#     "  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-#     "  `date` varchar(20) NOT NULL,"
-#     "  `protocol` varchar(40) NOT NULL,"
-#     "  `plugin` varchar(20) NOT NULL,"
-#     "  `remote_host` varchar(20) NOT NULL,"
-#     "  `remote_port` varchar(20) NOT NULL,"
-#     "  `data` LONGTEXT,"
-#     "  PRIMARY KEY (`time`)"
-#     ") ENGINE=InnoDB")
-#
-
 # db = mysql.connector.connect(**config)
 # cursor = db.cursor()
 #
---
+++ database/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE connections (
+	  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  date varchar(20) NOT NULL,
+	  protocol varchar(40) NOT NULL,
+	  plugin varchar(20) NOT NULL,
+	  remote_host varchar(20) NOT NULL,
+	  remote_port varchar(20) NOT NULL,
+	  data LONGTEXT,
+	  PRIMARY KEY (time));
+
+

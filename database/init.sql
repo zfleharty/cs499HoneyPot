4c8ce523720b802094a8eec4273c3028e2eff887
zfleharty <zfleharty@unm.edu>
2019-03-30 21:39:24 -0400

    changed plugin column of database to accept LONGTEXT

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE connections (
 	  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	  date varchar(20) NOT NULL,
 	  protocol varchar(40) NOT NULL,
-	  plugin varchar(20) NOT NULL,
+	  plugin LONGTEXT NOT NULL,
 	  remote_host varchar(20) NOT NULL,
 	  remote_port varchar(20) NOT NULL,
 	  data LONGTEXT,

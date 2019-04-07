70ee4d97e0df04e9afbc3156a9938a82fde1d853
zfleharty <zfleharty@unm.edu>
2019-04-07 19:30:22 -0400

    changed database to allow null values and longtext instead

--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE connections (
 	  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	  date varchar(20) NOT NULL,
-	  protocol varchar(40) NOT NULL,
-	  plugin LONGTEXT NOT NULL,
+	  date LONGTEXT NOT NULL,
+	  protocol LONGTEXT,
+	  plugin LONGTEXT,
 	  remote_host varchar(20) NOT NULL,
 	  remote_port varchar(20) NOT NULL,
 	  data LONGTEXT,
